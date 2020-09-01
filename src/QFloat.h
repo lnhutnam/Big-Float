7ad999df6f0f982f31ec3400e26f978c3db82634
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-01 18:45:49 +0700

    Update QFloat.h

--- src/QFloat.h
+++ src/QFloat.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 
 // constant
 #define _QFLOAT_8_BIT_EXPONENT 4
@@ -27,23 +30,28 @@
 // Structure
 struct _qfloat8_ {
 	// sign: 1 bit - exponent: 4 bit - fraction: 3 bit
+	unsigned char data = 0;
 };
 
 struct _qfloat16_ {
 	// sign: 1 bit - exponent: 5 bit - fraction: 10 bit
+	unsigned char data[2] = { 0 };
 };
 
 struct _qfloat32_ {
 	// sign: 1 bit - exponent: 8 bit - fraction: 23 bit
+	unsigned char data[4] = { 0 };
 };
 
 struct _qfloat64_ {
 	// sign: 1 bit - exponent: 11 bit - fraction: 52 bit
+	unsigned char data[8] = { 0 };
 };
 
 
 struct _qfloat80_ {
 	// sign: 1 bit - exponent: 15 bit - fraction: 64 bit
+	unsigned char data[10] = { 0 };
 };
 
 struct _qfloat128_ {
@@ -218,4 +226,97 @@ std::string _round(std::string s, int numberRound) {
 		}
 	}
 	return result;
+}
+
+// Qfloat file IO processing
+/*
+Parse function
+@param string line is the string need to parse
+@param string seperator is the separator string
+@return an array of subline has parsed by seperator
+*/
+std::vector<std::string> parse_string_by(std::string line, std::string seperator)
+{
+	std::vector<std::string> tokens;
+	int startPosition = 0;
+	size_t foundPosition = line.find(seperator, startPosition);
+
+	while (foundPosition != std::string::npos) {
+		int count = foundPosition - startPosition;
+		std::string token = line.substr(startPosition, count);
+
+		tokens.push_back(token);
+
+		startPosition = foundPosition + seperator.length();
+
+		foundPosition = line.find(seperator, startPosition);
+	}
+	int count = line.length() - startPosition;
+	std::string token = line.substr(startPosition, count);
+	tokens.push_back(token);
+	return tokens;
+}
+
+
+struct qfloatDataInput {
+	int baseSource = 0;
+	int baseDestination = 0;
+	std::string data = "";
+};
+
+/*
+readData function
+@param string source is the string source in file input
+@functional: convert the string source to qfloatDataInput
+*/
+qfloatDataInput readData(std::string source) {
+	qfloatDataInput result;
+	result.baseSource = 0;
+	result.baseDestination = 0;
+	result.data = "";
+	std::vector<std::string> parseData = parse_string_by(source, " ");
+	result.baseSource = std::atoi(parseData[0].c_str());
+	result.baseDestination = std::atoi(parseData[1].c_str());
+	result.data = parseData[2];
+	return result;
+}
+
+/*
+processing function
+@param string inputFilePath is the path of file input
+@param string outputFilePath is the path of file output
+*/
+void processing(std::string inputFilePath, std::string outputFilePath) {
+	std::ifstream inputStreamFile;
+	std::ofstream outputStreamFile;
+	inputStreamFile.open(inputFilePath);
+	if (inputStreamFile.fail()) {
+		std::cout << "The path file doesn't exist." << std::endl;
+		return;
+	}
+	outputStreamFile.open(outputFilePath);
+	if (outputStreamFile.fail()) {
+		std::cout << "The path file doesn't exist." << std::endl;
+		return;
+	}
+	while (!inputStreamFile.eof()) {
+		std::string temp = "";
+		std::getline(inputStreamFile, temp);
+		qfloatDataInput _data = readData(temp);
+		_qfloat128_ qfloat;
+		if (_data.baseSource == 2) {
+			_data.data = qfloat.decToBin();
+		}
+		if (_data.baseSource == 10) {
+			qfloat.scanQfloat(_data.data);
+		}
+		if (_data.baseDestination == 2) {
+			outputStreamFile << qfloat.decToBin() << std::endl;
+		}
+		if (_data.baseDestination == 10) {
+			outputStreamFile << qfloat.printQfloat() << std::endl;
+		}
+	}
+	inputStreamFile.close();
+	outputStreamFile.close();
 }
